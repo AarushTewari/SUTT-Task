c83ba7f3a908861fdbe565ff493a57d2010eeeca
AarushTewari <116949096+AarushTewari@users.noreply.github.com>
2023-06-22 04:38:45 +0530

    Update urls.py

--- hospital/urls.py
+++ hospital/urls.py
@@ -8,10 +8,11 @@ urlpatterns = [
     path('welcome/', views.welcome, name='welcome'),
     path('accounts/logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('change_profile', views.changeprofile, name='changeprofile'),
-    path('profile', views.profile, name='profile'),
+    path('profile', views.profile, name='patient_profile'),
     path('staff_login', views.staff_login, name='staff_login'),
     path('logout', views.logout, name='logout'),
     path('staffwelcome', views.welcomestaff, name='welcomestaff'),
     path('add_appointment', views.add_appointment, name='add_appointment'),
-    path('view_appointment', views.view_appointment, name='view_appointment')
+    path('view_appointment', views.view_appointment, name='view_appointment'),
+    path('search/', views.search_patients, name='search_users'),
 ]
