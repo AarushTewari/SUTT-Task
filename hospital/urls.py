826cf264cc5e879d46d4a2c0c49a0118d2f35fa4
AarushTewari <116949096+AarushTewari@users.noreply.github.com>
2023-06-22 22:57:47 +0530

    Update urls.py

--- hospital/urls.py
+++ hospital/urls.py
@@ -2,13 +2,15 @@ from django.urls import path
 from . import views
 from django.contrib.auth.views import LogoutView
 
+app_name = 'hospital'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('login', views.patient_login, name='login'),
     path('welcome/', views.welcome, name='welcome'),
     path('accounts/logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('change_profile', views.changeprofile, name='changeprofile'),
-    path('profile', views.profile, name='patient_profile'),
+    path('profile/<str:patient_name>', views.profile, name='profile'),
     path('staff_login', views.staff_login, name='staff_login'),
     path('logout', views.logout, name='logout'),
     path('staffwelcome', views.welcomestaff, name='welcomestaff'),
