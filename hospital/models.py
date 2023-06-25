1a17374da4cbf10d353da15b84b3a921ee0ec44b
AarushTewari <116949096+AarushTewari@users.noreply.github.com>
2023-06-25 16:25:48 +0530

    Update models.py

--- hospital/models.py
+++ hospital/models.py
@@ -1,20 +1,7 @@
 from django.db import models
 from django.utils import timezone
+from datetime import datetime
 # Create your models here.
-
-class Staff(models.Model):
-    roles = [
-        ('oncologist', 'oncologist'),
-        ('ent', 'ent'),
-        ('cardiologist', 'cardiologist'),
-        ('neurologist', 'neurologist'),
-        ('orthologist', 'orthologist')
-    ]
-    name = models.CharField(max_length=50)
-    role = models.CharField(choices=roles, max_length=20)    
-
-    def __str__(self):
-       return self.name
 
 class Patient(models.Model):
     name = models.CharField(max_length=50)
@@ -32,6 +19,10 @@ class Appointment(models.Model):
         ('completed', 'Completed'),
         ('cancelled', 'Cancelled'),
     ]
+    billing_status = [
+        ('pending', 'Pending'),
+        ('completed', 'Completed')
+    ]
     roles = [
         ('oncologist', 'oncologist'),
         ('ent', 'ent'),
@@ -45,9 +36,20 @@ class Appointment(models.Model):
     complain = models.TextField(default='No')
     doctor = models.CharField(choices=roles, max_length=30)
     status = models.CharField(choices=STATUS_CHOICES, max_length=30, default='pending')
+    cost = models.PositiveIntegerField(default=0)
+    billing_status = models.CharField(choices=billing_status, max_length=30, default='pending')
 
     def __str__(self):
        return self.patient.name
     
-class Bill(models.Model):
-    amount = models.IntegerField()
+class Room(models.Model):
+    name = models.CharField(max_length=1000)
+    patient = models.CharField(max_length=200, null=True)
+    def __str__(self):
+       return self.patient
+
+class message(models.Model):
+    value = models.CharField(max_length=100000000)
+    date = models.DateTimeField(default= datetime.now, blank=True)
+    user = models.CharField(max_length=100000000)
+    room = models.CharField(max_length=100000000, blank=True)
