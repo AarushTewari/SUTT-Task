364d6866e6aec8f201fbaa5dcb8c696e1f32ea2c
AarushTewari <116949096+AarushTewari@users.noreply.github.com>
2023-06-29 09:18:01 +0530

    Update urls.py

--- hospitalmanagement/urls.py
+++ hospitalmanagement/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('hospital.urls')),
-    path('', include('allauth.urls'))
+    path('', include('allauth.urls')),
 ]
