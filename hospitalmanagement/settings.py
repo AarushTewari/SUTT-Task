93660bafb1e9282cac6475ed06c053bd8165eb0d
AarushTewari <116949096+AarushTewari@users.noreply.github.com>
2023-06-29 09:56:02 +0530

    Update settings.py

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path 
+from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5w7u%-8bo(volt$n+==rrvxgtgsml0j&&cd&=w76%%=5aiwq2c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['aarushtewari.pythonanywhere.com']
 
 
 # Application definition
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
     'fuzzywuzzy',
     'django_tables2',
-    'crispy_forms',
 ]
 
 SITE_ID = 2
