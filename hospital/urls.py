2cc344c76f37cb4bf5ba534dcc6e320dde11f0c3
AarushTewari <116949096+AarushTewari@users.noreply.github.com>
2023-06-29 09:20:31 +0530

    Update urls.py

--- hospital/urls.py
+++ hospital/urls.py
@@ -6,14 +6,14 @@ app_name = 'hospital'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login', views.patient_login, name='login'),
+    path('login/', views.patient_login, name='login'),
     path('welcome/', views.welcome, name='welcome'),
     path('accounts/logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('change_profile', views.changeprofile, name='changeprofile'),
     path('profile/<int:id>', views.profile, name='profile'),
     path('patient_appointments', views.patient_appointments, name='patient_appointments'),
     path('staff_login', views.staff_login, name='staff_login'),
-    path('logout', views.Logout, name='logout'),
+    path('logout/', views.Logout, name='logout'),
     path('staffwelcome', views.welcomestaff, name='welcomestaff'),
     path('add_appointment', views.add_appointment, name='add_appointment'),
     path('view_appointment', views.view_appointment, name='view_appointment'),
@@ -25,5 +25,6 @@ urlpatterns = [
     path('chatrooms/<str:patient>', views.room, name='room'),
     path('send', views.send, name='send'),
     path('getMessages/<str:patient>/', views.getMessages, name='getMessages'),
-    path('staff_dashboard/', views.staff_dashboard, name='staff_dashboard')
+    path('staff_dashboard/', views.staff_dashboard, name='staff_dashboard'),
+    path('register/', views.register, name='register')
 ]
